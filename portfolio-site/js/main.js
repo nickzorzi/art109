8c78232deeb968d6d6aedfa7a75ad3774dcd6d42
nickzorzi <nicolas.zorzi@sjsu.edu>
2024-11-10 00:39:07 -0800

    portfolio edits for 11/10/24

--- portfolio-site/js/main.js
+++ portfolio-site/js/main.js
@@ -9,7 +9,7 @@ import * as THREE from 'three';
 import { OrbitControls } from 'https://unpkg.com/three@0.162.0/examples/jsm/controls/OrbitControls.js';
 //import { GLTFLoader } from 'https://unpkg.com/three@0.162.0/examples/jsm/loaders/GLTFLoader.js'; // to load 3d models
 
-let scene, camera, renderer, cube, sphere, controls, particlesMesh;
+let scene, camera, renderer, cube, cube2, cube3, sphere, controls, particlesMesh;
 
 const clock = new THREE.Clock();
 
@@ -27,29 +27,59 @@ function init() {
     renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.querySelector("#three-container").appendChild(renderer.domElement);
+
+    //knot "cube"
     
-    const geometry = new THREE.TorusKnotGeometry( 10, 1, 300, 32 );
+    const geometry = new THREE.TorusKnotGeometry( 10, 0.5, 450, 32 );
     // material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
     // const texture = new THREE.TextureLoader().load('../textures/ice002_1K-JPG_Color.jpg');
     // const material = new THREE.MeshBasicMaterial({ map: texture });
     const material = new THREE.PointsMaterial({
         size: 0.01,
-        color: 'yellow'
+        color: '#f6f930'
     });
     cube = new THREE.Points( geometry, material );
     // cube = new THREE.Mesh( geometry, material );
     scene.add( cube );
 
-    const geometry2 = new THREE.SphereGeometry( 2, 32, 16 );
-    // const texture2 = new THREE.TextureLoader().load('../textures/grasslight-big.jpg');
-    // const material2 = new THREE.MeshBasicMaterial( { map: texture2 } ); 
-    const material2 = new THREE.PointsMaterial({
+    //cube 2
+
+    const geometry3 = new THREE.TorusKnotGeometry( 15, 1, 600, 48, 2, 1 );
+    const material3 = new THREE.PointsMaterial({
         size: 0.01,
-        color: 'red'
+        color: '#f6f930'
     });
-    // sphere = new THREE.Mesh( geometry2, material2 );
-    sphere = new THREE.Points( geometry2, material2 );
-    scene.add( sphere );
+    cube2 = new THREE.Points( geometry3, material3 );
+    cube2.rotateY(2);
+    cube2.rotateX(5);
+    cube2.rotateZ(5);
+    scene.add( cube2 );
+
+    //cube 3
+
+    const geometry4 = new THREE.TorusKnotGeometry( 20, 1.5, 750, 64, 4, 3 );
+    const material4 = new THREE.PointsMaterial({
+        size: 0.01,
+        color: '#4ea5d9'
+    });
+    cube3 = new THREE.Points( geometry4, material4 );
+    cube2.rotateY(5);
+    cube3.rotateX(2);
+    cube3.rotateZ(2);
+    scene.add( cube3 );
+
+    //sphere
+
+    // const geometry2 = new THREE.SphereGeometry( 2, 32, 16 );
+    // // const texture2 = new THREE.TextureLoader().load('../textures/grasslight-big.jpg');
+    // // const material2 = new THREE.MeshBasicMaterial( { map: texture2 } ); 
+    // const material2 = new THREE.PointsMaterial({
+    //     size: 0.01,
+    //     color: 'red'
+    // });
+    // // sphere = new THREE.Mesh( geometry2, material2 );
+    // sphere = new THREE.Points( geometry2, material2 );
+    // scene.add( sphere );
 
     //space particles
 
@@ -106,16 +136,30 @@ function animate() {
     //     sphere.rotation.y += 0.01;
     // }
 
-    if (cube && sphere) {
-        cube.rotation.x = scrollY * .001;
-        sphere.rotation.y = scrollY * .001;
-    }
-
     const elapsedTime = clock.getElapsedTime();
 
+    if (cube && cube2) {
+        // cube.rotation.x = scrollY * .001;
+        // cube2.rotation.x = scrollY * .001;
+        // cube3.rotation.x = scrollY * .001;
+        // sphere.rotation.y = scrollY * .001;
+
+        cube.rotation.x = (-mouseX * 0.05) * (elapsedTime * 0.001);
+        cube.rotation.y = (mouseY * 0.05) * (elapsedTime * 0.001);
+
+        cube2.rotation.x = (-mouseX * 0.05) * (elapsedTime * 0.001);
+        cube2.rotation.y = (-mouseY * 0.05) * (elapsedTime * 0.001);
+
+        cube3.rotation.x = (mouseX * 0.05) * (elapsedTime * 0.001);
+        cube3.rotation.y = (-mouseY * 0.05) * (elapsedTime * 0.001);
+
+        // sphere.rotation.x = (mouseX * 0.05) * (elapsedTime * 0.001);
+        // sphere.rotation.y = (mouseY * 0.05) * (elapsedTime * 0.001);
+    }
+
     if (particlesMesh) {
-        particlesMesh.rotation.y = (-mouseY * 0.1) * (elapsedTime * 0.001);
-        particlesMesh.rotation.x = (-mouseX * 0.1) * (elapsedTime * 0.001);
+        particlesMesh.rotation.y = (mouseX * 0.05) * (elapsedTime * 0.001);
+        particlesMesh.rotation.x = (mouseY * 0.05) * (elapsedTime * 0.001);
     }
 
     //controls.update();
